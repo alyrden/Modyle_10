50f2e4778b193c61e941d8e42b28c7984dd0de63
alyrden <f50knox@gmail.com>
2025-01-05 23:48:16 +0300

    10

--- Modyle_10.1.py
+++ Modyle_10.1.py
@@ -6,7 +6,7 @@ def write_words(word_count, file_name):
 
     with open(file_name, 'w', encoding='utf-8') as f:
         for i in range(1, word_count + 1):
-            f.write(f"Какое-то слово № {i}\n ")
+            f.write(f"Какое-то слово № {i}\n")
             sleep(0.1)
     print(f"Завершилась запись в файл {file_name}")
 
